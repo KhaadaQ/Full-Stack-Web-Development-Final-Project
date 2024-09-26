dfb914f84c63339a68b6580a7d132b854b45306a
Daniel <khaadaqq@gmail.com>
2024-09-27 01:32:46 +0200

    FE_ItemPage_fix

--- Frontend/src/pages/ItemsPage.jsx
+++ Frontend/src/pages/ItemsPage.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const ItemsPage = () => {
     const [items, setItems] = useState([]);
+    const navigate = useNavigate();
     useEffect(() => {
         const fetchItems = async () => {
             try {
@@ -9,12 +11,13 @@ const ItemsPage = () => {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
-                        Authorization: `Bearer ${localStorage.getItem('token')}`, 
+                        Authorization: `Bearer ${localStorage.getItem('token')}`,
                     },
                 });
 
                 const data = await response.json();
-                setItems(data.items);
+                console.log(data);
+                setItems(data.item_classes || []);
             } catch (error) {
                 console.error('Error al obtener los items:', error);
             }
@@ -22,19 +25,24 @@ const ItemsPage = () => {
 
         fetchItems();
     }, []);
+    const goToDashboard = () => {
+        navigate('/dashboard');
+    };
     return (
         <div>
-      <h1>Items y Loot</h1>
-      {items.length > 0 ? (
-        <ul>
-          {items.map((item) => (
-            <li key={item.id}>{item.name}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No se encontraron items.</p>
-      )}
-    </div>
+            <h1>Items y Loot</h1>
+            {items.length > 0 ? (
+                <ul>
+                    {items.map((item) => (
+                        <li key={item.id}>{item.name}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No se encontraron items.</p>
+            )}
+
+            <button onClick={goToDashboard}>Volver a Dashboard</button>
+        </div>
     )
 }
 
